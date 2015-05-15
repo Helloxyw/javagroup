5632077cbb1b9222cb3f849191c7f18b354df2b3
Helloxyw <1641137052@qq.com>
2015-05-15 19:48:16 +0800

    task0001

---
+++ task/task0001/xyw/Javagroup/src/com/Compare.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Compare {
+	public static void main(String[] args) {
+		String str="Hello World";
+		String anotherString="hello world";
+		Object objStr=str;
+		
+		System.out.println(str.compareTo(anotherString));
+		System.out.println(str.compareToIgnoreCase(anotherString));
+		System.out.println(str.compareTo(objStr.toString()));
+		
+	}
+
+}
